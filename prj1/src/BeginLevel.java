248628f9a2128da5046486b03bac74820ed5eab3
Alek <Alektzster@gmail.com>
2017-09-17 22:34:56 +0300

    123

---
+++ prj1/src/BeginLevel.java
@@ -0,0 +1,14 @@
+public class BeginLevel {
+
+	public double circleLenght(double diameter) {
+		double cl = diameter * Math.PI;
+		return cl;
+	}
+
+	public double point_to_point(double pointX1, double pointY1, double pointX2, double pointY2) {
+
+		
+		return 0;
+	}
+
+}
